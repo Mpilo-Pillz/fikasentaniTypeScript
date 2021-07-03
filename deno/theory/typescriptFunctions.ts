e4feb8f0330aa5f66fbe8f818878b17f00de7f64
mpilopillz <mpilo.dlamini@yahoo.com>
2021-07-03 21:25:17 +0200

    leanring how to set a callback as an arguemnt type for a function

--- deno/theory/typescriptFunctions.ts
+++ deno/theory/typescriptFunctions.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
   console.log("Result:" + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(10, 100));
 console.log(printResult(add(10, 100))); //in JS this is undefined
 
@@ -14,3 +19,7 @@ let combinedValues: (num1: number, num2: number) => number;
 combinedValues = add;
 
 console.log(combinedValues(70500, 85000));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
